ca6d160084c4023b7bf6fa9ada6db0f0cea6e013
Việt Blogger <40515052+vietblogdao@users.noreply.github.com>
2018-11-17 10:17:13 +0700

    Update format.js

--- format.js
+++ format.js
@@ -3,14 +3,14 @@ self.addEventListener('message', function(e) {
   if (e.data.beautify) {
     self._window = self.window;
     self.window = {};
-    self.importScripts('{{ "/assets/js/lib/js-beautify/beautify.min.js" | relative_url }}');
+    self.importScripts('{{ "https://cdn.rawgit.com/vietblogdao/js/master/beautify.min.js" | relative_url }}');
     source = self.window.js_beautify(source, {
       unescape_strings: true,
       jslint_happy: true
     });
     self.window = self._window;
   }
-  self.importScripts('{{ "/assets/js/lib/highlight-js/highlight.pack.js" | relative_url }}');
+  self.importScripts('{{ "https://cdn.rawgit.com/vietblogdao/js/master/highlight.pack.js" | relative_url }}');
   source = self.hljs.highlight('javascript', source).value;
   source = source.split('\n');
   source = source.join('</code><code>');
